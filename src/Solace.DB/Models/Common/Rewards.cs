fd61298478c20ca1c0de4d117d9329dc608bc35f
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2026-06-04 12:04:45 +0200

    Implement gethashcode and equals for Rewards

--- src/Solace.DB/Models/Common/Rewards.cs
+++ src/Solace.DB/Models/Common/Rewards.cs
@@ -2,7 +2,6 @@
 
 namespace Solace.DB.Models.Common;
 
-// todo: implement gethashcode and equals
 public sealed record Rewards(
     int Rubies,
     int ExperiencePoints,
@@ -14,4 +13,34 @@ public sealed record Rewards(
 {
     public Rewards DeepCopy()
         => new Rewards(Rubies, ExperiencePoints, Level, new Dictionary<string, int?>(Items), [.. Buildplates], [.. Challenges]);
+
+    public bool Equals(Rewards? other)
+        => other is not null && Rubies == other.Rubies && ExperiencePoints == other.ExperiencePoints && Level == other.Level && Items.OrderBy(item => item.Key).Select(item => (item.Key, item.Value)).SequenceEqual(other.Items.OrderBy(item => item.Key).Select(item => (item.Key, item.Value))) && Buildplates.SequenceEqual(other.Buildplates) && Challenges.SequenceEqual(other.Challenges);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        hash.Add(Rubies);
+        hash.Add(ExperiencePoints);
+        hash.Add(Level);
+
+        foreach (var item in Items.OrderBy(item => item.Key))
+        {
+            hash.Add(item.Key);
+            hash.Add(item.Value);
+        }
+
+        foreach (var item in Buildplates)
+        {
+            hash.Add(item);
+        }
+
+        foreach (var item in Challenges)
+        {
+            hash.Add(item);
+        }
+
+        return hash.ToHashCode();
+    }
 }
